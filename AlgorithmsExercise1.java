717842e57a4f96f280fcc369b25411831256e0c3
Vaibhavi Mukadam <91684766+vaibhavimukadam@users.noreply.github.com>
2022-01-16 11:24:44 -0500

    Update AlgorithmsExercise1.java

--- AlgorithmsExercise1.java
+++ AlgorithmsExercise1.java
@@ -16,7 +16,6 @@ public class AlgorithmsExercise1 {
 		c) true -> boolean
 		d) 33 -> 1+2 as in and 3 as String 
  */
-/*
 	// 1.1.3 Write a program that takes three integer command-line arguments and prints equal if all three are equal, and not equal otherwise.
 		int int1 = Integer.parseInt(args[0]);
 		int int2 = Integer.parseInt(args[1]);
@@ -26,7 +25,7 @@ public class AlgorithmsExercise1 {
 			System.out.print("Equal");
 		else
 			System.out.print("Not Equal");
-
+/*
 	//1.1.4
 		a. if (a > b) then c = 0; =>No keyword called "then"
 		b. if a > b { c = 0; } => No parenthesis for if statement
@@ -35,7 +34,7 @@ public class AlgorithmsExercise1 {
 */
 		
 	// 1.1.5 Write a code fragment that prints true if the double variables x and y are both  strictly between 0 and 1 and false otherwise.
-		/*
+		
 		double x,y;
 		
 		System.out.println("Enter two variable");
@@ -45,10 +44,8 @@ public class AlgorithmsExercise1 {
 			System.out.print("True");
 		else
 			System.out.print("False");
-*/
 		
 	//1.1.6 : manually solved
-		/*
 		int f = 0; 
 		int g = 1; 
 		for (int i = 0; i <= 15; i++) 
@@ -57,13 +54,11 @@ public class AlgorithmsExercise1 {
 			f = f + g;
 			g = f - g; 
 		}
-	*/
 		//1.1.7 : manually solved. 
 		
 		//1.1.8 b, 'b' +'c' ... 197 as ascii value of a is 96, b is 97, c is 98, e ... 'a' + 4 results in e
 		
 		//1.1.9 binary value
-		/*
 		int k = 5 ;
 		System.out.println(Integer.toBinaryString(k));
 		
@@ -75,12 +70,10 @@ public class AlgorithmsExercise1 {
 			posint = posint/2;
 			}
 		System.out.print(s);
-			*/
 		//1.1.10 What is wrong with the following code fragment? 
 		// to fix  		int[] a = new int[10];
 		
 		//1.1.11
-		/*
 		boolean[][] barr = {{true, false, true},{false, true, true}};
 		
 		for (int i = 0; i < barr.length; i++) {
@@ -91,9 +84,7 @@ public class AlgorithmsExercise1 {
 					System.out.print(" ");
 				}
 			System.out.println();
-		}
-			*/
-		
+		}		
 		//1.1.12 What does the following code fragment print? >> there is only one print statement  0 to 9 loop printed
 		
 		//1.1.13 transpose 
